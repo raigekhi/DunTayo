4b64a6ad37d73f02fd499ee91618697a3a124187
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2017-04-09 13:49:47 +0800

    First Commit

---
+++ app/src/main/java/com/example/arvin/duntayo/ListGroup.java
@@ -0,0 +1,13 @@
+package com.example.arvin.duntayo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_group);
+    }
+}
